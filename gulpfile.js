c12b404578800cde9908f6b3d6ee4b8209954dee
Marco Alvarado <marcoantonioam9@gmail.com>
2016-06-06 23:24:51 -0700

    Adds sass support.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var stylish = require('gulp-jscs-stylish');
+var sass = require('gulp-sass');
 
 gulp.task('default', ['watch']);
 
@@ -18,6 +19,13 @@ gulp.task('jscs', function() {
     .pipe(stylish());
 });
 
+gulp.task('build-css', function() {
+  return gulp.src('app/styles/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/assets/css'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('app/scripts/**/*.js', ['jshint', 'jscs']);
+  gulp.watch('app/styles/main.scss', ['build-css']);
 });
